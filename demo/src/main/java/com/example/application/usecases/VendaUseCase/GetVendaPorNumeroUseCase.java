f037e3a9ad1aeffc298b3404123f70f7bdbd097d
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2025-11-23 21:08:15 -0300

    implementação do endpoint venda pelo seu numero

--- demo/src/main/java/com/example/application/usecases/VendaUseCase/GetVendaPorNumeroUseCase.java
+++ demo/src/main/java/com/example/application/usecases/VendaUseCase/GetVendaPorNumeroUseCase.java
@@ -1,4 +1,22 @@
 package com.example.application.usecases.VendaUseCase;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.domain.entities.Venda;
+import com.example.infrastructure.repositories.VendaRepository;
+
+@Service
 public class GetVendaPorNumeroUseCase {
+    
+    private final VendaRepository vendaRepository;
+    
+    public GetVendaPorNumeroUseCase(VendaRepository vendaRepository) {
+        this.vendaRepository = vendaRepository;
+    }
+    
+    public Optional<Venda> execute(Long numVenda) {
+        return vendaRepository.findByIdWithRelations(numVenda);
+    }
 }
--- demo/src/main/java/com/example/presentation/controllers/VendaController.java
+++ demo/src/main/java/com/example/presentation/controllers/VendaController.java
@@ -1,25 +1,38 @@
 package com.example.presentation.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.application.usecases.VendaUseCase.CalcularValorFinalVendaUseCase;
+import com.example.application.usecases.VendaUseCase.GetVendaPorNumeroUseCase;
+import com.example.domain.entities.Venda;
 
 @RestController
 @RequestMapping("/acmetech")
 public class VendaController {
     
     private final CalcularValorFinalVendaUseCase calcularValorFinalVendaUseCase;
+    private final GetVendaPorNumeroUseCase getVendaPorNumeroUseCase;
     
-    public VendaController(CalcularValorFinalVendaUseCase calcularValorFinalVendaUseCase) {
+    public VendaController(CalcularValorFinalVendaUseCase calcularValorFinalVendaUseCase,
+                           GetVendaPorNumeroUseCase getVendaPorNumeroUseCase) {
         this.calcularValorFinalVendaUseCase = calcularValorFinalVendaUseCase;
+        this.getVendaPorNumeroUseCase = getVendaPorNumeroUseCase;
     }
     
     @GetMapping("/vendas/{num}/valorfinal")
     public Double getValorFinalVenda(@PathVariable Long num) {
         return calcularValorFinalVendaUseCase.execute(num);
     }
+    
+    @GetMapping("/vendas/{num}")
+    public Optional<Venda> getVendaPorNumero(@PathVariable Long num) {
+        return getVendaPorNumeroUseCase.execute(num);
+    }
 }
+
 
