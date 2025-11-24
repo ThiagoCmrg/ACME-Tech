07a30ef6e5ca3650d32f62ce6dac0d7290e842b5
Nathan <nathan.schmitt@srconsulting.io>
2025-11-23 22:45:11 -0300

    implementando endpoint tecnologias por fornecedor

--- demo/src/main/java/com/example/presentation/controllers/TecnologiaController.java
+++ demo/src/main/java/com/example/presentation/controllers/TecnologiaController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.application.usecases.TecnologiaUseCase.GetAllTecnologiasUseCase;
 import com.example.application.usecases.TecnologiaUseCase.GetTecnologiaUseCase;
+import com.example.application.usecases.TecnologiaUseCase.GetTecnologiasPeloFornecedorUseCase;
 import com.example.domain.entities.Tecnologia;
+
 
 
 @RestController
@@ -16,10 +18,12 @@ public class TecnologiaController {
 
     private final GetTecnologiaUseCase getTecnologiaUseCase;
     private final GetAllTecnologiasUseCase getAllTecnologiasUseCase;
+    private final GetTecnologiasPeloFornecedorUseCase getTecnologiasPeloFornecedorUseCase;
 
-    public TecnologiaController(GetTecnologiaUseCase getTecnologiaUseCase, GetAllTecnologiasUseCase getAllTecnologiasUseCase) {
+    public TecnologiaController(GetTecnologiaUseCase getTecnologiaUseCase, GetAllTecnologiasUseCase getAllTecnologiasUseCase, GetTecnologiasPeloFornecedorUseCase getTecnologiasPeloFornecedorUseCase) {
         this.getTecnologiaUseCase = getTecnologiaUseCase;
         this.getAllTecnologiasUseCase = getAllTecnologiasUseCase;
+        this.getTecnologiasPeloFornecedorUseCase = getTecnologiasPeloFornecedorUseCase;
     }
 
     @GetMapping("/buscatecnologia/{id}")
@@ -31,5 +35,11 @@ public class TecnologiaController {
     public List<Tecnologia> listaTecnologias() {
         return getAllTecnologiasUseCase.getAllTecnologias();
     }
+
+    @GetMapping("/tecnologiaspelofornecedor/{cod}")
+    public List<Tecnologia> getMethodName(@PathVariable Long cod) {
+        return getTecnologiasPeloFornecedorUseCase.getTecnologiaByFornecedorCod(cod);
+    }
+    
     
 }
